5d70fffeb8cb4c6089c607d934fe835408f214e9
Matthew Polsom <matthewpolsom@hotmail.com>
2019-03-02 15:00:00 -0600

    Basic core file structure set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './core/home/home.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { LandingComponent } from './core/landing/landing.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+    { path: '', component: HomeComponent }, // Default page if user is logged in
+    { path: '', component: LandingComponent }, // Default page if user is not logged in
+    { path: '**', component: NotFoundComponent }, // Wildcard path, 404 error
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './core/home/home.component';
+import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './core/header/header.component';
 import { LandingComponent } from './core/landing/landing.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
-import { DashboardComponent } from './home/dashboard/dashboard.component';
 
 @NgModule({
-    declarations: [AppComponent, HomeComponent, HeaderComponent, LandingComponent, NotFoundComponent, DashboardComponent],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        HeaderComponent,
+        LandingComponent,
+        NotFoundComponent,
+    ],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
