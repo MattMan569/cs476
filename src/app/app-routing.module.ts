a35982f1cb51b529562fdcf3c77d32e7250f91ec
Matthew Polsom <matthewpolsom@hotmail.com>
2019-03-02 23:26:55 -0600

    Some routing work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,10 +9,18 @@ import { LandingComponent } from './core/landing/landing.component';
 
 // Custom services
 import { AuthGuard } from './auth/auth-guard.service';
+import { CalendarComponent } from './home/calendar/calendar.component';
+import { MessagesComponent } from './home/messages/messages.component';
+import { ProjectsComponent } from './home/projects/projects.component';
+import { UsersComponent } from './home/users/users.component';
 
 const routes: Routes = [
     { path: '', component: LandingComponent }, // Default page if user is not logged in
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] }, // Default page if user is logged in
+    { path: 'calendar', component: CalendarComponent, canActivate: [AuthGuard] }, // Default page if user is logged in
+    { path: 'messages', component: MessagesComponent, canActivate: [AuthGuard] }, // Default page if user is logged in
+    { path: 'projects', component: ProjectsComponent, canActivate: [AuthGuard] }, // Default page if user is logged in
+    { path: 'users', component: UsersComponent, canActivate: [AuthGuard] }, // Default page if user is logged in
     { path: '**', component: NotFoundComponent }, // Wildcard path, 404 error
     // { path: '**', redirectTo: 'home' }, // Wildcard path, redirect home
 ];
